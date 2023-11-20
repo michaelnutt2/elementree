eb1cafa2ae3c8c9e7a0f1e7780afde56d91758b7
Michael Nutt <michaeltnutt@gmail.com>
2023-11-20 14:38:15 -0600

    Added more tests
    Added tests for the new utility functions and updated the test data  with values that I had figured out the octree structure for by hand.

--- tests/unit/test_octree.py
+++ tests/unit/test_octree.py
@@ -24,26 +24,58 @@ class OctreeTestCase(unittest.TestCase):
     def setUp(self):
         from elementree.lidar.octree import Octree as oc
         self.array = np.array([
-            [1.52, 2.456, 0.516, 5],
-            [5.213, 2.16, 1.566, 6],
-            [3.15, 4.21, -1.25, 7]
+            [-5, 2, 3, 5],
+            [-5, -1, 4, 5],
+            [-4, 3, -1, 6],
+            [-4, 5, -5, 2],
+            [-3, 2, 1, 4],
+            [-3, -2, 5, 5],
+            [-2, 1, 4, 7],
+            [-2, 3, -2, 8],
+            [-1, 2, 3, 5],
+            [-1, 3, 4, 8],
+            [0, -1, 2, 2],
+            [0, 3, -4, 5],
+            [1, 2, 5, 4],
+            [1, 3, -1, 4],
+            [2, 4, -1, 0],
+            [2, -5, 2, 0],
+            [3, 0, 1, 0],
+            [4, -1, 5, 1],
+            [4, -2, 3, 1],
+            [5, 0, 2, 2],
+            [5, -3, 2, 5]
         ])
-        self.tree = oc.Octree(self.array, 16)
+        self.tree = oc.Octree(self.array, 8)
 
     def test_scale_to_range(self):
         self.tree._scale_to_range()
-        self.assertEqual(0, self.tree._point_cloud[0][0])
-        self.assertEqual(9462, self.tree._point_cloud[0][1])
-        self.assertEqual(41099, self.tree._point_cloud[0][2])
-        self.assertEqual(5, self.tree._point_cloud[0][3])
-        self.assertEqual(65535, self.tree._point_cloud[1][0])
-        self.assertEqual(0, self.tree._point_cloud[1][1])
-        self.assertEqual(65535, self.tree._point_cloud[1][2])
-        self.assertEqual(6, self.tree._point_cloud[1][3])
-        self.assertEqual(28925, self.tree._point_cloud[2][0])
-        self.assertEqual(65535, self.tree._point_cloud[2][1])
-        self.assertEqual(0, self.tree._point_cloud[2][2])
-        self.assertEqual(7, self.tree._point_cloud[2][3])
+        self.assertEqual(0, self.tree._points[0][0])
+        self.assertEqual(178, self.tree._points[0][1])
+        self.assertEqual(204, self.tree._points[0][2])
+        self.assertEqual(0, self.tree._points[1][0])
+        self.assertEqual(102, self.tree._points[1][1])
+        self.assertEqual(229, self.tree._points[1][2])
+        self.assertEqual(25, self.tree._points[2][0])
+        self.assertEqual(204, self.tree._points[2][1])
+        self.assertEqual(102, self.tree._points[2][2])
+        self.assertEqual(25, self.tree._points[3][0])
+        self.assertEqual(255, self.tree._points[3][1])
+        self.assertEqual(0, self.tree._points[3][2])
+
+    def test_downscale(self):
+        from elementree.lidar.octree.Octree import downscale
+        self.tree._scale_to_range()
+        x, y, z, _ = self.array[0]
+        x = downscale(x, 3)
+        y = downscale(y, 3)
+        z = downscale(z, 3)
+        self.assertEqual(0, x)
+        self.assertEqual(176, y)
+        self.assertEqual(200, z)
+
+    def test_build_tree(self):
+        self.tree._build_tree()
 
 
 if __name__ == '__main__':
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -23,35 +23,66 @@ class UtilsTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
         self.array = np.array([
-            [1.52, 2.456, 0.516, 5],
-            [5.213, 2.16, 1.566, 6],
-            [3.15, 4.21, -1.25, 7]
+            [-5, 2, 3, 5],
+            [-5, -1, 4, 5],
+            [-4, 3, -1, 6],
+            [-4, 4, -5, 2],
+            [-3, 2, 1, 4],
+            [-3, -2, 5, 5],
+            [-2, 1, 4, 7],
+            [-2, 3, -2, 8],
+            [-1, 2, 3, 5],
+            [-1, 3, 4, 8],
+            [0, -1, 2, 2],
+            [0, 3, -4, 5],
+            [1, 2, 5, 4],
+            [1, 3, -1, 4],
+            [2, 4, -1, 0],
+            [2, -3, 2, 0],
+            [3, 0, 1, 0],
+            [4, -1, 5, 1],
+            [4, -2, 3, 1],
+            [5, 0, 2, 2],
+            [5, -3, 2, 5]
         ])
 
     def test_find_bounds(self):
         from elementree.utils import find_bounds
         x_max, x_min, y_max, y_min, z_max, z_min = find_bounds(self.array)
-        self.assertEqual(x_max, 5.213)
-        self.assertEqual(x_min, 1.52)
-        self.assertEqual(y_max, 4.21)
-        self.assertEqual(y_min, 2.16)
-        self.assertEqual(z_max, 1.566)
-        self.assertEqual(z_min, -1.25)
+        self.assertEqual(5,  x_max)
+        self.assertEqual(-5, x_min)
+        self.assertEqual(4, y_max)
+        self.assertEqual(-3, y_min)
+        self.assertEqual(5, z_max)
+        self.assertEqual(-5, z_min)
 
     def test_scaling_factor_k_equals_16(self):
         from elementree.utils import scaling_factor
-        to_max = 2**16 - 1
+        to_max = 2**8 - 1
         to_min = 0
         x = self.array[:, 0]
         from_min = np.min(x)
         from_max = np.max(x)
         y = (scaling_factor(from_max, from_min, to_max, to_min))
-        self.assertEqual(y, 17745.7352)
+        self.assertEqual(25.5, y)
 
     def test_scale_x_to_range_k_equals_16(self):
         from elementree.utils import scale_to_range
-        x = self.array[:, 0]
+        x = self.array[0]
         scaled_16 = scale_to_range(np.float32(65535), np.float32(0.0), x)
-        self.assertEqual(scaled_16[0], 0)
-        self.assertEqual(scaled_16[1], 65535)
-        self.assertEqual(scaled_16[2], 28925)
+        self.assertEqual(0, scaled_16[0])
+        self.assertEqual(45874, scaled_16[1])
+        self.assertEqual(52428, scaled_16[2])
+
+    def test_scale_x_to_range_k_equals_8(self):
+        from elementree.utils import scale_to_range
+        x = self.array[0]
+        scaled_8 = scale_to_range(np.float32(255), np.float32(0.0), x)
+        self.assertEqual(0, scaled_8[0])
+        self.assertEqual(178, scaled_8[1])
+        self.assertEqual(204, scaled_8[2])
+
+    def test_find_center_point(self):
+        from elementree.utils import find_center_point
+        cp = find_center_point(0, 255)
+        self.assertEqual(127, cp)
