83e9129840eb335eedc928eb9f84b73b345e6698
Michael Nutt <michaeltnutt@gmail.com>
2023-11-20 14:39:14 -0600

    Renamed Constants and added some values
    Changed the file name to cfg.py for easier referencing, and added some more constants for accessing x, y, and z easier.

---
+++ src/elementree/lidar/octree/cfg.py
@@ -0,0 +1,27 @@
+# ------------------------------------------------------------------------------
+#      ElemenTree - Python library of tree data structures
+#      Copyright (C) 2023  Michael Nutt
+#
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ------------------------------------------------------------------------------
+
+X_MAX: int = 0
+X_MIN: int = 1
+Y_MAX: int = 2
+Y_MIN: int = 3
+Z_MAX: int = 4
+Z_MIN: int = 5
+X: int = 0
+Y: int = 1
+Z: int = 2
